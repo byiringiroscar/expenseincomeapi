c3f715b7f259d48375e22ea5f55ed0f7527ce7f6
unknown <byiringoroscar@gmail.com>
2022-02-06 13:57:35 +0200

    change something in manage

--- manage.py
+++ manage.py
@@ -6,8 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expensive.settings')
-    # os.environ['DJANGO_SETTINGS_MODULE'] = 'expensive.settings'
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expensive.settings')
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'expensive.settings'
 
     try:
         from django.core.management import execute_from_command_line
